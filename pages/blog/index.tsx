f393b45e58ed0134d207506416bf06e7aab27e30
Ares <ccccodechef@gmail.com>
2022-09-05 01:50:36 +0200

    Animate blog items

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,6 +1,7 @@
 import { Blog } from '~/components';
 import { getAllPostsFrontMatter } from '~/lib/post';
 import { Layout } from '~/layouts';
+import { Animate } from '~/components';
 
 import type { GetStaticProps } from 'next';
 
@@ -31,10 +32,23 @@ export default function BlogPage({ serialisedFrontmatters }: BlogProps) {
 		<Layout.Default seo={{ title: 'nuro ─ blog' }}>
 			<div className="mt-8 sm:mt-16 mb-20 mx-0 sm:mx-6 lg:mb-28 lg:mx-8">
 				<div className="relative max-w-6xl mx-auto">
-					<Blog.Latest frontmatter={latestPost} />
+					<Animate
+						animation={{ y: [50, 0], opacity: [0, 1] }}
+						key={0}
+					>
+						<Blog.Latest frontmatter={latestPost} />
+					</Animate>
 					<div className="mt-4 lg:mt-12 grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:max-w-none">
 						{frontmatters.map((frontmatter, i) => (
-							<Blog.Post key={i} frontmatter={frontmatter} index={i} />
+							<Animate
+								animation={{ y: [50, 0], opacity: [0, 1] }}
+								key={i + 1}
+								transition={{
+									delay: 0.1 * (i + 1),
+								}}
+							>
+								<Blog.Post key={i} frontmatter={frontmatter} index={i} />
+							</Animate>
 						))}
 					</div>
 				</div>
