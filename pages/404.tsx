c69c15a810fd231dd913379ef652099cdb8aa609
Ares <ccccodechef@gmail.com>
2022-09-06 02:11:20 +0200

    Shorter 404 error title

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,5 @@
 import { ErrorPage } from '~/components';
 
 export default function Error() {
-	return <ErrorPage title="Looks like you took a wrong turn" message="The page you're looking for couldn't be found." icon="feather:alert-triangle" />;
+	return <ErrorPage title="You took a wrong turn" message="The page you're looking for couldn't be found." icon="feather:alert-triangle" />;
 }
